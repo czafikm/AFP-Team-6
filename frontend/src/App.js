ed8edd0e182bfc37f2e5cf01b4914e7573c21ef3
Evelin <komjatievelin@gmail.com>
2020-11-10 14:39:00 +0100

    component watch listazasara

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import ComplexNumberRecordingForm from "./component/ComplexNumberRecordingForm";
 import ComplexNumberList from "./component/ComplexNumberList";
 import WatchRecordingForm from "./component/WatchRecordingForm";
+import WatchList from "./component/WatchList";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <div className={"col-md-6"}>
 
                 <WatchRecordingForm/>
+                <WatchList/>
             </div>
             <div className={"col-md-3"}></div>
         </div>
